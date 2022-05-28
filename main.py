5a87020df2a2e25699279c97c8d8ccba80dc4182
Parsival Nascimento <92957914+Dev-Parsival@users.noreply.github.com>
2022-05-28 16:26:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome ");
+ano = int(input("Em qual ano vc nasceu: "));
+anoAtual = int(input("Em qual ano estamos? "));
+calculo = int(anoAtual - ano);
+print()
+print(f'Olá {nome} sua idade é {calculo}')
